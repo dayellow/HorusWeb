6adb0871f31258fb24264a49b5e6ac7ab98571a1
Constantine3 <472416450@qq.com>
2020-07-30 16:42:10 +0800

    cors

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from horus_app import create_app
+from flask_cors import *
 
 app = create_app('config')
+CORS(app, supports_credentials=True)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(PORT=5000, HOST="0.0.0.0")
